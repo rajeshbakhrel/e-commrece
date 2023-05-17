7a5ecd5f795fc545240f60598675ddfe3a29de7e
rajeshbakhrel <rajeshbakhrel@gmail.com>
2023-05-17 14:55:28 +0545

    commit

--- src/Components/GridView.jsx
+++ src/Components/GridView.jsx
@@ -1,26 +1,17 @@
-import React from 'react'
-import SingleCart from './SingleCart'
-import "../Style/GridView.css"
+import React from "react";
+import SingleCart from "./SingleCart";
+import "../Style/GridView.css";
 
-
-const GridView = ({product}) => {
+const GridView = ({ product }) => {
   return (
-    <div className='full-grid-view-container'>
-      
-      <div className='grid-view-single-cart'>
-
-        {
-          product.map((currElem, id)=>{
-
-            return(
-                <SingleCart key={currElem.id} {...currElem}/>
-            )
-
-          })
-        }
+    <div className="full-grid-view-container">
+      <div className="grid-view-single-cart">
+        {product.map((currElem, id) => {
+          return <SingleCart key={currElem.id} {...currElem} />;
+        })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default GridView
+export default GridView;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,35 +1,27 @@
-import React from 'react'
-import "../Style/Product.css"
-import ProductFilter from '../Components/ProductFilter'
-import ProductUpperFilter from '../Components/ProductUpperFilter'
-import ProductContainer from '../Components/ProductContainer'
-
+import React from "react";
+import "../Style/Product.css";
+import ProductFilter from "../Components/ProductFilter";
+import ProductUpperFilter from "../Components/ProductUpperFilter";
+import ProductContainer from "../Components/ProductContainer";
 
 const Products = () => {
-
-
-
   return (
-    <div className='product-page-full-container'>
-        <div className="filter-side-full-container">
-            <ProductFilter/>
-        </div>
-
-        <div className="product-display-side">
-            <div className="product-display-upper-filter">
-              <ProductUpperFilter/>
-            </div>
-            
-            <div className="product-display-lower-container">
-              <ProductContainer/>
-            </div>
-
-        </div>
-      
-      
-      
+    <div className="product-page-full-container">
+      <div className="filter-side-full-container">
+        <ProductFilter />
       </div>
-  )
-}
 
-export default Products
+      <div className="product-display-side">
+        <div className="product-display-upper-filter">
+          <ProductUpperFilter />
+        </div>
+
+        <div className="product-display-lower-container">
+          <ProductContainer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
